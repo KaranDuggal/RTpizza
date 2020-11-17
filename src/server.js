d74e7a6cd8ee450224c9b2e82120051efa7375cd
KaranDuggal <duggalkaran400@gmail.com>
2020-11-17 14:12:11 +0530

    initial

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// ==== Paths ====
+const viewsPath = path.join(__dirname,'../resources/views');
+console.log(viewsPath);
+
+// ==== set templates engine ====
+app.set('view engine', 'ejs');
+app.set('views', viewsPath);
+// app.use(expressLayout);
+// app.use(express.static(staticFiles));
+
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs');
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`);
+});
